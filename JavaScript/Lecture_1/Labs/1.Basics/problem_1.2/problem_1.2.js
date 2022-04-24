ba65c202e5bc147cb9fd5a7185f3694b04d893ad
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-04-24 18:05:37 +0200

    Refactor problem 1.2 and add comments

--- JavaScript/Lecture_1/Labs/1.Basics/problem_1.2/problem_1.2.js
+++ JavaScript/Lecture_1/Labs/1.Basics/problem_1.2/problem_1.2.js
@@ -1,13 +1,36 @@
+/**
+ * numbers from the user
+ */
 let numbers = [];
 
 while (true) {
-  const userIn = prompt("Enter a valid number");
+  /**
+   * Prompt the user for a number
+   */
+  const userIn = prompt(
+    `Enter a valid number (prev numbers are ${numbers.join(", ") || "-- "})`
+  );
+  /**
+   * Convert the number(string) user entered to number(number)
+   */
   const number = parseInt(userIn);
+  /**
+   * If the number is not valide repeat again
+   */
   if (isNaN(number)) continue;
-  numbers.push(number);
+  /**
+   * If the number is not 0 add it to the numbers array
+   */
+  if (number != 0) numbers.push(number);
+  /**
+   * Calculate the sum of the user entered numbers
+   */
   const sum = numbers.reduce((sum, number) => sum + number, 0);
+  /**
+   * If the number is 0 or the sum exceeds 100 break the loop and print the sum
+   */
   if (number == 0 || sum > 100) {
-    alert(`The sum of numbers is ${sum}`);
+    document.write(`<h1>The sum of numbers is ${sum}</h1>`);
     break;
   }
 }
