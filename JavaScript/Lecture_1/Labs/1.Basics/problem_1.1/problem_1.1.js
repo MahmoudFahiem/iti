5f22f98f397ce19026348a39a167032724282ec2
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-04-24 13:34:55 +0200

    Refactor problem 1.1 and add comments

--- JavaScript/Lecture_1/Labs/1.Basics/problem_1.1/problem_1.1.js
+++ JavaScript/Lecture_1/Labs/1.Basics/problem_1.1/problem_1.1.js
@@ -1,5 +1,21 @@
+/**
+ * Prompt the user for a message
+ */
 const message = prompt("Enter a message");
+/**
+ * Prepare template for view
+ */
+let viewTemp = "<h1>Heading</h1><hr>";
 
+/**
+ * Loop through all possible headings
+ * and append it to the viewTemp variable
+ */
 for (let i = 1; i <= 6; i++) {
-  document.write(`<h${i}>${message}</h${i}>`);
+  viewTemp += `<h${i}>${message}</h${i}>`;
 }
+
+/**
+ * Write the view template in the document
+ */
+document.write(viewTemp);
