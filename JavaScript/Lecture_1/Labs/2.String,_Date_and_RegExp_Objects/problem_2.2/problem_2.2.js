1421f1c6bcbb7c924a573fc72f1e0c0cafc84f7b
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-04-24 18:14:23 +0200

    Add comments to problem 2.2

--- JavaScript/Lecture_1/Labs/2.String,_Date_and_RegExp_Objects/problem_2.2/problem_2.2.js
+++ JavaScript/Lecture_1/Labs/2.String,_Date_and_RegExp_Objects/problem_2.2/problem_2.2.js
@@ -1,9 +1,27 @@
+/**
+ * Prompt the user to enter a word to check if it is palindrome
+ */
 const userIn = prompt("Enter a word to check if it palindrome or not");
+/**
+ * Prompt the user if he wants to consider case sensitive in check
+ */
 const isCaseSen = confirm("Do want to consider case sensitive?");
+/**
+ * If case sensitive is not considered convert all letters to lowercase (or uppercase)
+ */
 const word = isCaseSen ? userIn : userIn.toLowerCase();
+/**
+ * Reverse the word
+ */
 const reversedWord = word.split("").reverse().join("");
+/**
+ * Check if the reversed word equals the entered word
+ */
 const isPalindrome = word == reversedWord;
 
+/**
+ * Print the results in the view
+ */
 document.write(
   `<h1>The word ${userIn} is${isPalindrome ? " " : " not "}palindrome</h1>`
 );
