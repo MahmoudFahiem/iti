d8d376cc47c60157da9b0f44a70a8aed6a14d6a0
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-04-26 00:22:02 +0200

    Add problem 4.1 solution

---
+++ JavaScript/Lecture_2/Labs/4.Math_Object/problem_4.1/problem_4.1.js
@@ -0,0 +1,24 @@
+/**
+ * Define constant PI
+ */
+const PI = Math.PI;
+/**
+ * Get radius of a circle from user
+ *
+ * @returns {number} circleRad
+ */
+const getCircleRad = () => {
+  while (true) {
+    const userIn = prompt("Enter the value of a circle radius");
+    const circleRad = parseInt(userIn);
+    if (!isNaN(circleRad)) return circleRad;
+  }
+};
+
+/**
+ * Get radius after validation and calculate area of the circle
+ */
+const circleRad = getCircleRad();
+const circleArea = PI * circleRad ** 2;
+
+alert(`Total area of the circle is ${circleArea.toFixed(2)} m^2`);
